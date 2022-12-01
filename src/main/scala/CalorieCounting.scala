a201182f8548bd21478c6d882b9304509299ffed
Roman Mendaliev <reaheight@gmail.com>
2022-12-01 19:24:18 +0300

    Day 1 solution

---
+++ src/main/scala/CalorieCounting.scala
@@ -0,0 +1,8 @@
+object CalorieCounting extends AOCSolution[Integer, Integer](1) {
+  private val calories = inputBlocks(_.toInt).map(_.sum).sorted
+
+  protected val FirstPartAnswer = calories.last
+  protected val SecondPartAnswer = calories.takeRight(3).sum
+}
+
+@main def main() = CalorieCounting.printAnswers()
